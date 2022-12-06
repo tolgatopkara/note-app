7655cbd2efd3abc7d47f1a52e4845f3835565185
Tolga <tolga.topkara96@gmail.com>
2022-12-06 22:06:04 +0300

    standalone-appmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone : true,
+  template : `
+  <router-outlet></router-outlet>
+  <h3>Hello Angular</h3>
+  `,
+  styles : [  ],
+  imports : [ RouterModule],
+  providers: [ ]
 })
 export class AppComponent {
   title = 'note-app';
+
 }
